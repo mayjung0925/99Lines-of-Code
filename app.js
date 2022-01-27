8a09352e9defd5cb85a49cd80bfc4b3c2efc1fe8
Eunjin Jeoung <mayjung0925@gmail.com>
2022-01-27 16:37:24 -0600

    changed if else condition

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ for (let i = 0; i < friends.length; i++) {
     console.log (name.toUpperCase() + ':');
 
     for (let j = 99; j > 0; j--) {
-
-        if (j >= 2) {
-            let song1 = j + 'lines of code in the file,' + ' ' + j + ' ' + 'lines of code;' + ' ' + name + ' ' + 'strikes one out' + ' ' + 'clears it all out,';
+        if(j == 2){
+            song1 =  j + 'lines of code in the file,' + ' ' + j + ' ' + 'lines of code;' + ' ' + name + ' ' + 'strikes one out' + ' ' + 'clears it all out,'+ (j-1)+ ' '+ 'line of code in the file';
             console.log(song1);
-
-        } else {
-            let song2 =  j +' '+ 'line of code in the file,' + ' ' + j + ' ' + 'line of code;' + ' ' + name + ' ' + 'strikes one out' + ' ' + 'clears it all out,';
+        } else if( j == 1 ){
+            let song2 =  j +' '+ 'line of code in the file,' + ' ' + j + ' ' + 'line of code;' + ' ' + name + ' ' + 'strikes one out' + ' ' + 'clears it all out,' + ' '+'no more lines of code in the file';
             console.log(song2);
+        } else {
+            let song3 = j + 'lines of code in the file,' + ' ' + j + ' ' + 'lines of code;' + ' ' + name + ' ' + 'strikes one out' + ' ' + 'clears it all out,'+ (j-1)+ 'lines of code in the file';
+            console.log(song3);
         }
-    }
+
+        }
+
 }
